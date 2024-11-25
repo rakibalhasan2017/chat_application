4e2fe9a6926a0461cf6aafcc3acac36538eccf9e
rakib <rakuhasan2017@gmai.com>
2024-11-25 21:32:32 +0600

    login page is done

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -36,13 +36,43 @@ export const signup = async(req, res) => {
     console.log("something happened in sigup");
     res.status(400).send("signup error");
     console.log(error.message);
-    
-    
    }
 }
 
-export const login = (req, res) => {
-    res.status(201).send("login route");
+export const login = async(req, res) => {
+    const {email, password} = req.body;
+    try {
+        if(!email || !password) {
+       return res.status(400).send("fill all the fields");
+    }
+    const user = await User.findOne({email});
+    if(!user) {
+      return  res.status(400).send("this user is not signed in");
+    }
+    else {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send("Invalid password, password doesn't match.");
+        }
+        else {
+            generateToken(user._id, res);
+            res.status(200).json({
+                id: user._id,
+                fullname: user.fullname,
+                email: user.email,
+                profilepic: user.profilepic, // Assuming you have a profile picture field
+            });
+            console.log("login successfull");
+            
+        }
+    }
+    }
+    catch(error) {
+        console.log("something happened in login");
+        res.status(400).send("login error");
+        console.log(error.message);
+       }
+    
 }
 
 export const logout = (req, res) => {
